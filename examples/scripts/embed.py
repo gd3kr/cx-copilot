a2d9df77ddcf0e791e298fdb4b2a160ff10b0a26
Rahul <rahulsonwalkar@rahuls-mbp.lan>
2023-01-10 02:15:09 -0800

    add example script

--- examples/scripts/embed.py
+++ examples/scripts/embed.py
@@ -1,2 +1,7 @@
 from cx_copilot import OpenAIEmbeddingBlock, PineconeVectorDBBlock
-print(db.lookup(vec, 'readwise').ClosestEmbeddings)
+
+op = OpenAIEmbeddingBlock(open_ai_key='YOUR_OPEN_AI_KEY')
+vec = op.embed_text('hello, please help me reset my account password')
+db = PineconeVectorDBBlock("YOUR_PINECONE_KEY", "us-west1-gcp")
+
+print(db.lookup(vec, 'YOUR_PINECONE_INDEX').ClosestEmbeddings)
