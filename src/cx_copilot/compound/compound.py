1a188c68928313b00f5362d5e12c0a35b0ba0309
tanujtiwari98 <tanujtiwari98@gmail.com>
2023-01-11 14:15:08 -0800

    add openai config

--- src/cx_copilot/compound/compound.py
+++ src/cx_copilot/compound/compound.py
@@ -3,11 +3,16 @@ import redis
 from envyaml import EnvYAML
 from ..blocks.cache import Cache, RedisCache
 from ..blocks.tickets import ConversationRepository, HelpscoutConversationRepository
-
+from ..blocks.vectordb import VectorDBBlock, PineconeVectorDBBlock
+from ..blocks.completion import CompletionBlock, GPTCompletionBlock
+from ..blocks.embedding import EmbeddingBlock, OpenAIEmbeddingBlock
 
 class CXCopilot:
     cache_block: Cache
     ticket_repo: ConversationRepository
+    vector_db: VectorDBBlock
+    embedding: EmbeddingBlock
+    completion: CompletionBlock
 
     def __init_cache__(self, yaml: EnvYAML):
         cache_config: Optional[Dict] = yaml.get('cache')
@@ -30,7 +35,38 @@ class CXCopilot:
             secret = ticket_config['secret']
             self.ticket_repo = HelpscoutConversationRepository(app_id=app_id, app_secret=secret)
 
+    def __init_vector_db__(self, yaml: EnvYAML):
+        vector_config: Optional[Dict] = yaml.get('vectordb')
+        if vector_config is None:
+            return
+
+        if vector_config['type'] == 'pinecone':
+            key = vector_config['key']
+            env = vector_config['environment']
+            self.vector_db = PineconeVectorDBBlock(key=key, environment=env)
+
+    def __init_completion__(self, yaml: EnvYAML):
+        completion_config: Optional[Dict] = yaml.get('completion')
+        if completion_config is None:
+            return
+
+        if completion_config['type'] == 'openai':
+            key = completion_config['key']
+            self.completion = GPTCompletionBlock(open_ai_key=key)
+
+    def __init_embedding__(self, yaml: EnvYAML):
+        embedding_config: Optional[Dict] = yaml.get('embedding')
+        if embedding_config is None:
+            return
+
+        if embedding_config['type'] == 'openai':
+            key = embedding_config['key']
+            self.embedding = OpenAIEmbeddingBlock(open_ai_key=key)
+
     def __init__(self, path='copilot_config.yml'):
         env = EnvYAML(path)
         self.__init_cache__(env)
         self.__init_ticket_repo__(env)
+        self.__init_vector_db__(env)
+        self.__init_completion__(env)
+        self.__init_embedding__(env)
