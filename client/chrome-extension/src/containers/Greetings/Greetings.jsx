f61bd71af87d1edc624db2189dcc8ffeef3be3f9
Rahul <rahulsonwalkar23@gmail.com>
2023-01-12 21:23:09 -0800

    Add client/chrome-extension code

---
+++ client/chrome-extension/src/containers/Greetings/Greetings.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class GreetingComponent extends Component {
+  state = {
+    name: 'dev',
+  };
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default GreetingComponent;
