36aa9e0f9e332916009a788ea83d15cd0a4b2ff6
Alexander Kuo <alexkuo98@gmail.com>
2023-01-30 12:11:23 -0800

    ,

--- client/chrome-extension/src/components/citations.jsx
+++ client/chrome-extension/src/components/citations.jsx
@@ -37,7 +37,7 @@ const Citations = (props) => {
 								<>
 								<dt>
 									<Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
-									<span className="text-base font-semibold leading-7">{index+1}   ({citation.score})</span>
+									<span className="text-base font-semibold leading-7">{index+1} ({citation.score})</span>
 									<span className="ml-6 flex h-7 items-center">
 										{open ? (
 										<PlusSmallIcon className="h-6 w-6" aria-hidden="true" />
