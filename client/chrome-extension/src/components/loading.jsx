cb14e00f532b0df1642b356999e6630d76bc91a6
Alexander Kuo <alexkuo98@gmail.com>
2023-02-01 09:31:50 -0800

    .

--- client/chrome-extension/src/components/loading.jsx
+++ client/chrome-extension/src/components/loading.jsx
@@ -6,7 +6,7 @@ const Loading = () => {
 		<div className="md:flex md:items-center md:justify-between">
 			<div className="min-w-0 flex-1">
 				<h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-					Loading responses for ticket...
+					Loading...
 				</h2>
 			</div>
 		</div>
