23ec43cc985e13fdfa6a66592258995b0d30a761
AaryaGadekar <aaryagadekar01@gmail.com>
2023-02-03 15:32:19 +0530

    changes to make UI prettier

--- client/chrome-extension/src/components/nextCompletion.jsx
+++ client/chrome-extension/src/components/nextCompletion.jsx
@@ -1,28 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import '../styles/globals.css'
+import React, { useEffect, useState } from "react";
+import "../styles/globals.css";
 
 const NextCompletion = (props) => {
-  const {
-    completionIdx,
-    completionsLength,
-    setNextCompletionIdx,
-  } = props;
+  const { completionIdx, completionsLength, setNextCompletionIdx } = props;
 
   return (
-    <div className="flex space-x-2 justify-center py-4">
-    <span className="text-base font-semibold leading-7">{completionIdx+1}/{completionsLength}</span>
-    <div>
+    <div
+      className="flex
+      justify-center
+      items-center
+    "
+    >
+      <svg
+      className="cursor-pointer
+      fill-gray-400
+      "
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="25"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+        onClick={() => setNextCompletionIdx(true)}
+      >
+        <path
+          fill-rule="evenodd"
+          d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
+        />
+      </svg>
+      <span className="text-base font-semibold leading-7">
+        {completionIdx + 1}/{completionsLength}
+      </span>
+      <svg
+            className="cursor-pointer 
+            
+      fill-gray-400"
+
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="25"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+        onClick={() => setNextCompletionIdx(false)}
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
+        />
+      </svg>
+      {/* <div>
         <button
 					type="button"
-					onClick={setNextCompletionIdx}
+					onClick={()=>
+            setNextCompletionIdx(true)
+          }
 					className="inline-flex items-center rounded-md border border-transparent bg-blue-600 uppercase px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 					>
 					Next Suggestion
         </button>
+    </div> */}
     </div>
-    </div>
-  )
+  );
+};
 
-}
-
-export default NextCompletion
+export default NextCompletion;
--- client/chrome-extension/src/components/summary.jsx
+++ client/chrome-extension/src/components/summary.jsx
@@ -10,13 +10,19 @@ const Summary = (props) => {
 
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-7xl px-6 py-4 sm:py-32 lg:py-40 lg:px-8">
-        <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
-          <Disclosure as="div" key={"summary"} className="pt-6">
+      {/* <div className="mx-auto max-w-7xl px-6 py-4 sm:py-32 lg:py-40 lg:px-8">
+        <div className="mx-auto max-w-4xl divide-y divide-gray-900/10"> */}
+        <div
+        className='p-4 bg-white border border-gray-200 rounded-md shadow-sm divide-y divide-gray-200'
+        >
+          <Disclosure as="div" key={"summary"} className=""
+          defaultOpen={true}
+          >
             {({ open }) => (
               <>
                 <dt>
-                  <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
+                  <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900
+                  ">
                     <span className="text-base font-semibold leading-7">Summary</span>
                     <span className="ml-6 flex h-7 items-center">
                       {open ? (
@@ -27,14 +33,16 @@ const Summary = (props) => {
                     </span>
                   </Disclosure.Button>
                 </dt>
-                <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                  <p className="text-base leading-7 text-gray-600">{summary}</p>
+                <Disclosure.Panel as="dd" className="mt-2">
+                  <p className="text-base text-gray-500 break-words">{summary}</p>
+                  
                 </Disclosure.Panel>
               </>
             )}
           </Disclosure>
-        </div>
-      </div>
+          </div>
+        {/* </div>
+      </div> */}
     </div>
   )
 }
